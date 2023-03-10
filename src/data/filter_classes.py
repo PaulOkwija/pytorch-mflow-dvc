c7de45995ef5ea415870cc05fe73481ed9d4d005
Paul Mugume Okwija <okwijapaul@gmail.com>
2023-03-10 17:36:39 +0300

    Adjusting filter labels

--- src/data/filter_classes.py
+++ src/data/filter_classes.py
@@ -13,38 +13,38 @@ from utils.mlflow_run_decorator import mlflow_run
 def make_csv_path(excluded_labels=None):
 
     # List all the images in raw/ 
-    list_images = glob.glob('./data/raw/**/*.jpg', recursive=True)
-    df = pd.DataFrame({'path': list_images})
-    df['label'] = df['path'].apply(lambda x:os.path.basename(os.path.dirname(x)))
+    # list_images = glob.glob('./data/external/**/*.jpg', recursive=True)
+    df = pd.DataFrame('./data/external/Breast_cancer_dataset.csv')
+    # df['label'] = df['path'].apply(lambda x:os.path.basename(os.path.dirname(x)))
     print(df.shape)
 
     # Do we filter certain classes ? 
-    if excluded_labels is not None:
-        df = df[~df['label'].isin(excluded_labels)]
-        print('Csv file constituted with {nrows} images, representing {nclasses} classes, excluding {exclusion}'.format(
-        nrows=df.shape[0],
-        nclasses=len(df['label'].unique()),
-        exclusion= ', '.join(excluded_labels)
-        ))
-    else:
-        print('Csv file constituted with {nrows} images, representing the {nclasses} classes'.format(
-            nrows=df.shape[0],
-            nclasses=len(df['label'].unique())
-        ))
+    # if excluded_labels is not None:
+    #     df = df[~df['label'].isin(excluded_labels)]
+    #     print('Csv file constituted with {nrows} images, representing {nclasses} classes, excluding {exclusion}'.format(
+    #     nrows=df.shape[0],
+    #     nclasses=len(df['label'].unique()),
+    #     exclusion= ', '.join(excluded_labels)
+    #     ))
+    # else:
+    #     print('Csv file constituted with {nrows} images, representing the {nclasses} classes'.format(
+    #         nrows=df.shape[0],
+    #         nclasses=len(df['label'].unique())
+    #     ))
     
     mlflow.log_param("excluded_classes", excluded_labels)
     # Converting classes to number 
-    labels = df['label'].unique()
+    labels = df['class'].unique()
     labels.sort()
     labels_map = {label:i for i, label in enumerate(labels)}
-    df['y'] = df['label'].map(labels_map)
+    df['y'] = df['class'].map(labels_map)
     
     # Saving the map numbers -> label
     labels_map_save = {i:label for i, label in enumerate(labels)}
     with open("./data/processed/labels_map.yaml", "w") as outfile:
         yaml.dump(labels_map_save, outfile)
 
-    df.to_csv('./data/interim/filepath.csv', index=False)
+    # df.to_csv('./data/interim/filepath.csv', index=False)
 
 if __name__ == '__main__':
     
